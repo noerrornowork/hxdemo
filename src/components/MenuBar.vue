4b8deba5296ad1e6c9c0625e7a34237da9f1b945
noerrornowork <gao.yang@transn.com>
2020-03-27 01:13:24 +0800

    init commit

---
+++ src/components/MenuBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    顶部导航栏
+  </div>
+</template>
+  
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class MenuBar extends Vue {}
+</script>
+
+<style></style>
